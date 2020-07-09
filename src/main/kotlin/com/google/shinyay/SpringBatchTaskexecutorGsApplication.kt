fee0972e9c89d2e75523a5c366f3025b6ccd6b52
shinyay <shinya.com@gmail.com>
2020-07-09 22:27:41 +0900

    Add Enable Spring Batch

--- src/main/kotlin/com/google/shinyay/SpringBatchTaskexecutorGsApplication.kt
+++ src/main/kotlin/com/google/shinyay/SpringBatchTaskexecutorGsApplication.kt
@@ -1,9 +1,11 @@
 package com.google.shinyay
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableBatchProcessing
 class SpringBatchTaskexecutorGsApplication
 
 fun main(args: Array<String>) {
