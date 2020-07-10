c2bec753657147b6f91bc824fc5899e4fb561f99
shinyay <shinya.com@gmail.com>
2020-07-10 23:26:29 +0900

    Add Processor Class

---
+++ src/main/kotlin/com/google/shinyay/processor/EmployeeItempProcessor.kt
@@ -0,0 +1,14 @@
+package com.google.shinyay.processor
+
+import com.google.shinyay.entity.Employee
+import com.google.shinyay.logger
+import org.springframework.batch.item.ItemProcessor
+
+class EmployeeItempProcessor: ItemProcessor<Employee, Employee> {
+    override fun process(employee: Employee): Employee? {
+        val upperCasedEmployee = Employee(employee.firstName.toUpperCase(),
+        employee.lastName.toUpperCase())
+        logger.info("Uppercased [$employee] to [$upperCasedEmployee]")
+        return upperCasedEmployee
+    }
+}
