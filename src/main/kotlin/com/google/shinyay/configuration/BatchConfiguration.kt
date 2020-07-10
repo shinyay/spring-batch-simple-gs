49643d9db6490b161164caa4d93b80d7bc59082c
shinyay <shinya.com@gmail.com>
2020-07-10 23:41:11 +0900

    Add builder factory for Job and Step for Autowiring

--- src/main/kotlin/com/google/shinyay/configuration/BatchConfiguration.kt
+++ src/main/kotlin/com/google/shinyay/configuration/BatchConfiguration.kt
@@ -1,7 +1,10 @@
 package com.google.shinyay.configuration
 
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class BatchConfiguration {
+class BatchConfiguration(jobBuilderFactory: JobBuilderFactory,
+                         stepBuilderFactory: StepBuilderFactory) {
 }
