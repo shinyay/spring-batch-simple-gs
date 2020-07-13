95feaf009adfe1c3395694c10a18adfafb87d39a
shinyay <shinya.com@gmail.com>
2020-07-13 22:04:21 +0900

    Remove useless reader class

--- src/main/kotlin/com/google/shinyay/reader/SimpleItemReader.kt
+++
@@ -1,7 +0,0 @@
-package com.google.shinyay.reader
-
-import org.springframework.batch.item.ItemReader
-
-class SimpleItemReader(private val reader: ItemReader<Int>): ItemReader<Int> {
-    override fun read(): Int? = reader.read()
-}
